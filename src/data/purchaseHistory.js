5bd45a0c1f6415a1d3692351757207a9378beef5
Preetika Agrawal <preetika145btcse23@igdtuw.ac.in>
2025-02-14 16:27:13 +0530

    Update purchaseHistory.js

--- src/data/purchaseHistory.js
+++ src/data/purchaseHistory.js
@@ -6,7 +6,7 @@ const purchaseHistory = [
     name: "Eco-Friendly T-Shirt",
     description: "Made from 100% organic cotton, biodegradable fabric.",
     price: 499.99,
-    image_url: "https://github.com/Nandini-Pandey/updated/blob/5591b5f2f356b58a397c7ce6b9b7581fc4d78b60/src/product-images/numerical-methods.png",
+    image_url: "https://cdn11.bigcommerce.com/s-jjm7kgkrrc/images/stencil/1280x1280/products/43261/7046540/32460__23836.1735846886.jpg?c=1",
     payment_status: "Payment Successful",
     seller_name: "GreenThreads Store"
   },
@@ -17,7 +17,7 @@ const purchaseHistory = [
     name: "Reusable Water Bottle",
     description: "750ml stainless steel, BPA-free, keeps drinks cold for 12h.",
     price: 799.99,
-    image_url: "../product-images/numerical-methods.png",
+    image_url: "https://cdn11.bigcommerce.com/s-jjm7kgkrrc/images/stencil/1280x1280/products/43261/7046540/32460__23836.1735846886.jpg?c=1",
     payment_status: "Payment Successful",
     seller_name: "EcoHydrate Shop"
   }
