94fdcc5f7d341b13912f67abbf4c5f49fc0b6fd8
Nandini Pandey <pandeynandini2004@gmail.com>
2025-02-14 06:14:17 +0000

    spelling corrected

--- src/components/WishList.jsx
+++ src/components/WishList.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 import wishlistData from '../data/wishlistData';
 import './wishlist.css';
-import emptyWishlist from "../assets/empty-bro.png";
+import emptyWishlist from "../assets/Empty-bro.png";
 
 
 
