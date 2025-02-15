d11d4f8fa14861969c98c08ab195961ea40b80b4
Nandini Pandey <pandeynandini2004@gmail.com>
2025-02-15 15:45:43 +0000

    page not found error resolved

--- frontend/src/firebaseConfig.jsx
+++ frontend/src/firebaseConfig.jsx
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";  // Firebase Auth import
 import { getFirestore } from "firebase/firestore"; // Firestore import
 
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
